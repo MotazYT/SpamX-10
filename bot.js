0d110bc441885dcf4aae8413cc81c4a92af894ea
MotazYT <44732876+MotazYT@users.noreply.github.com>
2020-04-11 14:33:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
-const client5 = new Discord.Client();
 
 
 var prefix = "1";
@@ -16,18 +13,6 @@ client2.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
-client3.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
-
-client4.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
-
-client5.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
-
 client.on('ready', async() => {
 var server = "698465041158242306"; // ايدي السررفر
 var channel = "698494250261938217";//ايدي الروم
@@ -37,30 +22,6 @@ var channel = "698494250261938217";//ايدي الروم
 })
 
 client2.on('ready', async() => {
-var server = "698465041158242306"; // ايدي السررفر
-var channel = "698494250261938217";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-client3.on('ready', async() => {
-var server = "698465041158242306"; // ايدي السررفر
-var channel = "698494250261938217";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-client4.on('ready', async() => {
-var server = "698465041158242306"; // ايدي السررفر
-var channel = "698494250261938217";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-client5.on('ready', async() => {
 var server = "698465041158242306"; // ايدي السررفر
 var channel = "698494250261938217";//ايدي الروم
     setInterval(()=>{
@@ -99,57 +60,6 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
    message.delete()
   }
  });
- 
-client3.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
-client4.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
-client5.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
-client3.login(process.env.TOKEN3);
-client4.login(process.env.TOKEN4);
-client5.login(process.env.TOKEN5);
